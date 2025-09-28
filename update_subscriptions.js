b1b1e40286971fe1e23600efd258a072bbf0c25b
crashgfw <jiangnvmeng47@gmail.com>
2025-09-28 11:43:10 -0400

    improve docs

--- update_subscriptions.js
+++ update_subscriptions.js
@@ -1,10 +1,9 @@
-/* 
+/*
  *  run the scription locally
  *  node update_subscriptions.js ${xboard_api_secret} ${xboard_api_url}
  */
 
 function updateSubscriptions(credentials, xboard_api_url) {
-    
     console.log("Updating subscriptions...");
 
     fetch(
@@ -19,6 +18,7 @@ function updateSubscriptions(credentials, xboard_api_url) {
     )
         .then((response) => {
             if (!response.ok) {
+                console.error(response.status);
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             return response.json();
@@ -28,37 +28,38 @@ function updateSubscriptions(credentials, xboard_api_url) {
                 console.log("No subscriptions found");
                 throw new Error("No subscriptions found");
             }
-            data.data.forEach((item) => {
-                //console.log("item", item);
-                const now = new Date();
-                // 构造 "明天 10:00"
-                const today23 = new Date(
-                    now.getFullYear(),
-                    now.getMonth(),
-                    now.getDate(), // 明天
-                    23,                // 10 点
-                    0,
-                    0,
-                    0
-                );
-                
-                const today23Sec = Math.floor(today23.getTime() / 1000);
-                const nowSec = Math.floor(Date.now() / 1000); 
-                
-                if (item.total_used >= item.transfer_enable || item.expired_at < today23Sec) {
-                    //console.log(`id: ${item.id}`);
+            //console.log("item", item);
+            const now = new Date();
+            // 构造 "明天 10:00"
+            const today23 = new Date(
+                now.getFullYear(),
+                now.getMonth(),
+                now.getDate(), // 明天
+                23, // 10 点
+                0,
+                0,
+                0
+            );
 
-                    if(item.total_used >= item.transfer_enable){
-                        console.log("update due to traffic used up ", item.id)
+            const today23Sec = Math.floor(today23.getTime() / 1000);
+            const nowSec = Math.floor(Date.now() / 1000);
+
+            data.data.forEach((item) => {
+                if (
+                    item.total_used >= item.transfer_enable ||
+                    item.expired_at < today23Sec
+                ) {
+
+                    if (item.total_used >= item.transfer_enable) {
+                        console.log("update due to traffic used up ", item.id);
                         item.u = 0;
                         item.d = 0;
                     }
-                
-                    if( item.expired_at < today23Sec) {
-                        console.log("update due to expiring for ", item.id)
-                         // 在当前时间基础上增加 15天（15 * 24 * 60 * 60 秒）
+
+                    if (item.expired_at < today23Sec) {
+                        console.log("update due to expiring for ", item.id);
+                        // 在当前时间基础上增加 15天（15 * 24 * 60 * 60 秒）
                         item.expired_at = nowSec + 15 * 24 * 60 * 60;
-                        
                     }
 
                     fetch(`${xboard_api_url}/update`, {
@@ -69,23 +70,29 @@ function updateSubscriptions(credentials, xboard_api_url) {
                         },
                         body: JSON.stringify(item),
                     }).then((response) => {
-                        console.log("reset uuid & subscription url for:", item.id);
-                        fetch(
-                            `${xboard_api_url}/resetSecret`,
-                            {
-                                method: "POST",
-                                headers: {
-                                    Authorization: `${credentials}`,
-                                    "Content-Type": "application/json", // Adjust content type as needed
-                                },
-                                body: JSON.stringify({ id: item.id }),
-                            }
-                        )
+                        console.log(
+                            "reset uuid & subscription url for:",
+                            item.id
+                        );
+                        fetch(`${xboard_api_url}/resetSecret`, {
+                            method: "POST",
+                            headers: {
+                                Authorization: `${credentials}`,
+                                "Content-Type": "application/json", // Adjust content type as needed
+                            },
+                            body: JSON.stringify({ id: item.id }),
+                        })
                             .then((response) => {
-                                console.log("reset uuid & subscription url successfully for", item.id);
+                                console.log(
+                                    "reset uuid & subscription url successfully for",
+                                    item.id
+                                );
                             })
                             .catch((error) => {
-                                console.error("reset uuid & subscription url with error for", item.id);
+                                console.error(
+                                    "reset uuid & subscription url with error for",
+                                    item.id
+                                );
                             });
                     });
                 }
@@ -102,5 +109,3 @@ const credentials = process.argv[2];
 const xboardurl = process.argv[3];
 
 updateSubscriptions(credentials, xboardurl);
-
-
