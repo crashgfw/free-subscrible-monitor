ac71cfce2edb605d18a386e41094edec329cb287
crashgfw <jiangnvmeng47@gmail.com>
2026-05-07 20:44:14 -0400

    replace urls

--- generate_markdown4.js
+++ generate_markdown4.js
@@ -51,7 +51,7 @@ function fetchSubscriptions(credentials, xboard_api_url) {
             data.data.forEach((item) => {
                 subscriptions.push({
                     url:
-                        "https://fn08.sp0303.xyz/nodes/" +
+                        "https://fn08.sp0502.xyz/nodes/" +
                         item.token,
                 });
             });
